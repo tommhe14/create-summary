f4e232698c0594cf7b3629834a5d8251d1596b38
mane <53539004+tommhe14@users.noreply.github.com>
2024-10-10 16:58:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ AGENTS_FILE_PATH = 'agents_data.json'
 class FreshDesk:
     def __init__(self, api_key):
         self.api_link = API('streetsheaver-help.freshdesk.com', api_key)
-        genai.configure(api_key=os.environ["api_key"])
+        genai.configure(api_key=os.environ["API_KEY"])
         self.model = genai.GenerativeModel("models/gemini-1.5-pro-latest")
         self.agents = self.load_agents()
 
